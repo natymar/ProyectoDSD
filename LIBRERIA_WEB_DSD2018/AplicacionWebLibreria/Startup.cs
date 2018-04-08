398efd41514877aa2e05aaaf0044fc0f926f2af1
gpillaca91 <gpillaca91@gmail.com>
2018-04-08 10:01:24 -0500

    Gente la solucion y el proyecto para el from esta listo.. quedamos que sera en aspx. confirmen un commit todos porfa

---
+++ LIBRERIA_WEB_DSD2018/AplicacionWebLibreria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionWebLibreria.Startup))]
+namespace AplicacionWebLibreria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
