62e741529965b15477963109209cb48c8e1e0064
EMAYTA <elicita.mn@gmail.com>
2018-04-08 23:58:24 -0500

    add login
    add login

---
+++ LIBRERIA_WEB_DSD2018/WebAplicacionLibreria2018/Inicio.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html><html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Login Intranet</title>    
+        <link rel="stylesheet" href="style.css">    
+  </head>
+
+  <body background="fondo3.jpg" style="background-repeat: no-repeat;">
+
+    <div class="wrapper">
+	<div class="container">
+		<h1>Bienvenido</h1>
+		
+		<form id="Form1" class="form" runat="server">
+			    
+
+
+
+
+
+
+    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+
+
+    <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+
+
+
+
+
+    <asp:Button ID="btnNuevo" runat="server" onclick="btnNuevo_Click" 
+        Text="Entrar" />
+
+
+
+
+
+
+		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	    <li>
+            
+	</ul>
+</div>
+    <script src='js/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
+</html>
---
+++ LIBRERIA_WEB_DSD2018/WebAplicacionLibreria2018/Inicio.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control Form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
+        
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// Control btnNuevo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNuevo;
+    }
+}
