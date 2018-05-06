257be8ba4dc51c892ac7a3b917b0b97cbaed0b18
EMAYTA <elicita.mn@gmail.com>
2018-05-06 15:44:07 -0500

    commit para a entirdad produstos en REST

---
+++ LIBRERIA_WEB_DSD2018/WCFRESTPRDUCTOS/ServiceProductosREST.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFRESTPRDUCTOS
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServiceProductosREST" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServiceProductosREST.svc o ServiceProductosREST.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServiceProductosREST : IServiceProductosREST
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
